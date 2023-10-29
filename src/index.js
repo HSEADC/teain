83c070a2c74fe442482f524a10c3e62de36e88ac
Iaroslav Kukharenko <145665428+AnastasiaTolschinaDiz@users.noreply.github.com>
2023-10-29 11:58:01 +0300

    added responsiveness

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-import './index.css';
-console.log('Hello!')
+import './index.css'
+var form = document.getElementById('my-form')
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  var status = document.getElementById('my-form-status')
+  var data = new FormData(event.target)
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = 'Спасибо за подписку!'
+        form.reset()
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data['errors']
+              .map((error) => error['message'])
+              .join(', ')
+          } else {
+            status.innerHTML = 'Возникли проблемы с отправкой почты.'
+          }
+        })
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = 'Возникли проблемы с отправкой почты.'
+    })
+}
+form.addEventListener('submit', handleSubmit)
